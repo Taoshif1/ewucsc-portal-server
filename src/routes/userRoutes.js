2bb0f7036074efa9894c646a668c2327c963cfc7
Taoshif1 <taoshif2@gmail.com>
2026-09-23 01:49:08 +0600

    User Routes added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,15 +2,29 @@ import express from "express";
 import { createUser, loginUser } from "../controllers/userController.js";
 import { verifyFirebase } from "../middleware/verifyFirebase.js";
 import { verifyJWT } from "../middleware/verifyJWT.js";
+import { getUserCollection } from "../models/userModel.js";
 
 const router = express.Router();
 
 
-router.get("/profile", verifyJWT, (req, res) => {
-  res.send({
-    message: "Protected route working",
-    user: req.user
-  });
+router.get("/profile", verifyJWT, async (req, res) => {
+  try {
+    const users = await getUserCollection();
+
+    const user = await users.findOne({ uid: req.user.uid });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.send({
+      message: "Protected route working",
+      user,
+    });
+  } catch (error) {
+    console.error("Profile fetch error:", error);
+    res.status(500).send({ message: "Failed to fetch profile" });
+  }
 });
 
 router.post("/users", verifyFirebase, createUser);
