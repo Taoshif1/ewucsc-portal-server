fcd216b129d2c74dfa369461d1d29a5be977647a
Taoshif1 <taoshif2@gmail.com>
2026-09-25 02:10:44 +0600

    feat(content): mount announcements and blogs API

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import userRoutes from "./src/routes/userRoutes.js";
 import ctfRoutes from "./src/routes/ctfRoutes.js";
 import challengeRoutes from "./src/routes/challengeRoutes.js";
 import homeworkRoutes from "./src/routes/homeworkRoutes.js";
+import contentRoutes from "./src/routes/contentRoutes.js";
 
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use("/api", userRoutes);
 app.use("/api/ctf", ctfRoutes);
 app.use("/api/challenges", challengeRoutes);
 app.use("/api/homeworks", homeworkRoutes);
+app.use("/api/content", contentRoutes);
 
 app.get("/", (req, res) => {
   res.send("EWUCSC Server Running");
