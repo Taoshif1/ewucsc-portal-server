318766025b558e7dbf323b7dfc3397aa5f27a2ed
Taoshif1 <taoshif2@gmail.com>
2026-09-25 02:10:40 +0600

    feat(content): expose public and admin content APIs

---
+++ src/routes/contentRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  createContent,
+  listAllContent,
+  listPublishedContent,
+  updateContent,
+} from "../controllers/contentController.js";
+import { allowRoles } from "../middleware/allowRoles.js";
+import { verifyApprovedMember } from "../middleware/verifyApprovedMember.js";
+import { verifyJWT } from "../middleware/verifyJWT.js";
+
+const router = express.Router();
+
+router.get("/:type", listPublishedContent);
+
+router.get(
+  "/:type/admin/all",
+  verifyJWT,
+  verifyApprovedMember,
+  allowRoles("admin", "executive"),
+  listAllContent,
+);
+router.post(
+  "/:type/admin",
+  verifyJWT,
+  verifyApprovedMember,
+  allowRoles("admin", "executive"),
+  createContent,
+);
+router.patch(
+  "/:type/admin/:id",
+  verifyJWT,
+  verifyApprovedMember,
+  allowRoles("admin", "executive"),
+  updateContent,
+);
+
+export default router;
